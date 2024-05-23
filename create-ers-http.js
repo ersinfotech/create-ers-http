f2290103860d8c277831e947a3ee709715290bb6
uxnow <odootest2023@163.com>
2024-05-23 14:49:34 +0800

    refine

--- create-ers-http.js
+++ create-ers-http.js
@@ -5,19 +5,19 @@ const path = require('node:path')
 const { execSync } = require('node:child_process')
 
 const mkdirSync = (filepath) => {
-    if (fs.existsSync(filepath)) {
-        return
-    }
-    fs.mkdirSync(filepath, { recursive: true })
-    console.log(`created ${filepath}`)
+  if (fs.existsSync(filepath)) {
+    return
+  }
+  fs.mkdirSync(filepath, { recursive: true })
+  console.log(`created ${filepath}`)
 }
 
 const writeFileSync = (filepath, data) => {
-    if (fs.existsSync(filepath)) {
-        return
-    }
-    fs.writeFileSync(filepath, data)
-    console.log(`created ${filepath}`)
+  if (fs.existsSync(filepath)) {
+    return
+  }
+  fs.writeFileSync(filepath, data)
+  console.log(`created ${filepath}`)
 }
 
 const root = process.cwd()
@@ -26,8 +26,8 @@ console.log(root)
 const package_json_path = path.join(root, 'package.json')
 
 if (fs.existsSync(package_json_path)) {
-    console.log('package.json already existed, skip creating')
-    process.exit()
+  console.log('package.json already existed, skip creating')
+  process.exit()
 }
 
 mkdirSync('config')
@@ -43,10 +43,6 @@ module.exports = {
 }
 `)
 
-//writeFileSync('config/default-0.cjs', `
-//{}
-//`)
-//
 writeFileSync('.gitignore', `
 node_modules/
 config/*
@@ -57,7 +53,7 @@ writeFileSync('src/graphql/schema.js', `
 export default \`
 
 type Query {
-    echo(message: JSON!): JSON
+  echo(message: JSON!): JSON
 }
 
 \`
@@ -65,14 +61,15 @@ type Query {
 
 writeFileSync('src/graphql/API.js', `
 export default class API {
-    constructor (logger) {
-        this.logger = logger
-    }
+  constructor (logger) {
+    this.logger = logger
+  }
 
-    echo ({message}, {session}) {
-        console.log(session)
-        return message
-    }
+  echo ({message}, {session}) {
+    console.log(session)
+    console.log({message})
+    return message
+  }
 }
 `)
 
@@ -87,16 +84,16 @@ import API from './API.js'
 import resolver from './resolver.js'
 
 export default (logger) => {
-    const api = new API(logger)
-    return {api, schema, resolver}
+  const api = new API(logger)
+  return {api, schema, resolver}
 }
 `)
 
 writeFileSync('src/restful/index.js', `
 export default (logger, router, restrict) => {
-    router.use('/hello', (req, res) => {
-        res.end('hello world')
-    })
+  router.use('/hello', (req, res) => {
+    res.end('hello world')
+  })
 }
 `)
 
@@ -140,8 +137,8 @@ import graphql from './src/graphql/index.js'
 import restful from './src/restful/index.js'
 
 ehttp(config, {
-    graphql,
-    restful,
+  graphql,
+  restful,
 })
 `)
 
@@ -150,7 +147,7 @@ execSync('rm -rf node_modules/')
 execSync('npm init -y')
 
 const package = require(package_json_path)
-package.scripts.start = 'node-dev .'
+package.scripts.start = 'nodemon .'
 package.type = 'module'
 
 fs.writeFileSync(package_json_path, JSON.stringify(package, null, 2))
@@ -158,6 +155,6 @@ fs.writeFileSync(package_json_path, JSON.stringify(package, null, 2))
 console.log('please wait')
 
 execSync('npm i config ers-http')
-execSync('npm i -D node-dev')
+execSync('npm i -D nodemon')
 
 console.log(`\nyou can 'npm start' now`)
