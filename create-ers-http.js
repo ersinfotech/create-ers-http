e6a23bdf22418b2e82f4bf912b32a89f9282b3aa
odootest2023 <odootest2023@163.com>
2025-05-05 21:33:38 +0800

    use tsx

--- create-ers-http.js
+++ create-ers-http.js
@@ -34,22 +34,30 @@ mkdirSync('config')
 mkdirSync('src/graphql')
 mkdirSync('src/restful')
 
-writeFileSync('config/default.cjs', `
+writeFileSync(
+  'config/default.cjs',
+  `
 module.exports = {
   clientId: 'miner',
   eadmin: {
     baseUrl: 'http://api.ersinfotech.com/eadmin2-api',
   },
 }
-`)
+`,
+)
 
-writeFileSync('.gitignore', `
+writeFileSync(
+  '.gitignore',
+  `
 node_modules/
 config/*
 !config/default.cjs
-`)
+`,
+)
 
-writeFileSync('src/graphql/schema.js', `
+writeFileSync(
+  'src/graphql/schema.ts',
+  `
 export default \`
 
 type Query {
@@ -57,47 +65,55 @@ type Query {
 }
 
 \`
-`)
+`,
+)
 
-writeFileSync('src/graphql/API.js', `
-export default class API {
-  constructor (logger) {
-    this.logger = logger
-  }
-
-  echo ({message}, {session}) {
-    console.log(session)
-    console.log({message})
-    return message
-  }
+writeFileSync(
+  'src/graphql/api.ts',
+  `
+export const echo = async ({ message }, { session }) => {
+  console.log(session)
+  console.log({ message })
+  return message
 }
-`)
+`,
+)
 
-writeFileSync('src/graphql/resolver.js', `
+writeFileSync(
+  'src/graphql/resolver.ts',
+  `
 export default {
 }
-`)
+`,
+)
 
-writeFileSync('src/graphql/index.js', `
-import schema from './schema.js'
-import API from './API.js'
-import resolver from './resolver.js'
+writeFileSync(
+  'src/graphql/index.ts',
+  `
+import schema from './schema'
+import * as api from './api'
+import resolver from './resolver'
 
-export default (logger) => {
-  const api = new API(logger)
-  return {api, schema, resolver}
+export default () => {
+  return { api, schema, resolver }
 }
-`)
+`,
+)
 
-writeFileSync('src/restful/index.js', `
+writeFileSync(
+  'src/restful/index.ts',
+  `
 export default (logger, router, restrict) => {
   router.use('/hello', (req, res) => {
     res.end('hello world')
   })
 }
-`)
+`,
+)
 
-writeFileSync('ecosystem.config.cjs', `
+writeFileSync(
+  'ecosystem.config.cjs',
+  `
 //
 // 只修改env里面的内容即可，其他配置请保持不变
 // 专门用于pm2 nvm的联合使用
@@ -108,47 +124,57 @@ writeFileSync('ecosystem.config.cjs', `
 var pkg = require('./package.json')
 
 module.exports = {
-  apps : [{
-    name      : pkg.name,
-    script    : pkg.main,
-    interpreter: process.env.NVM_DIR + '/nvm-exec',
-    interpreter_args: 'node',
-    env: {
-      NODE_VERSION: 20,
-      PORT: 3000,
-      NODE_ENV: 'production',
+  apps: [
+    {
+      name: pkg.name,
+      script: pkg.main,
+      interpreter: process.env.NVM_DIR + '/nvm-exec',
+      interpreter_args: 'npx tsx',
+      env: {
+        NODE_VERSION: 16,
+        PORT: 3000,
+        NODE_ENV: 'production',
+      },
     },
-  }],
-};
-`)
+  ],
+}
+`,
+)
 
-writeFileSync('init.js', `
+writeFileSync(
+  'init.ts',
+  `
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 global.__base = path.dirname(fileURLToPath(import.meta.url))
 console.json = d => console.log(JSON.stringify(d, null, 2))
-`)
+`,
+)
 
-writeFileSync('index.js', `
-import './init.js'
+writeFileSync(
+  'index.ts',
+  `
+import './init'
 import config from 'config'
 import ehttp from 'ers-http'
-import graphql from '#src/graphql/index.js'
-import restful from '#src/restful/index.js'
+import graphql from '#src/graphql'
+import restful from '#src/restful'
 
 ehttp(config, {
   graphql,
   restful,
 })
-`)
+`,
+)
 
 execSync('cp config/default.cjs config/development.cjs')
 execSync('rm -rf node_modules/')
 execSync('npm init -y')
 
 const package = require(package_json_path)
-package.scripts.start = 'nodemon .'
+package.scripts.start = 'tsx watch .'
 package.type = 'module'
+package.main = 'index.ts'
 package.imports = package.imports || {}
 package.imports['#src/*'] = './src/*'
 
@@ -157,6 +183,6 @@ fs.writeFileSync(package_json_path, JSON.stringify(package, null, 2))
 console.log('please wait')
 
 spawnSync('npm', ['i', 'config', 'ers-http'], { stdio: 'inherit' })
-spawnSync('npm', ['i', '-D', 'nodemon'], { stdio: 'inherit' })
+spawnSync('npm', ['i', '-D', 'tsx'], { stdio: 'inherit' })
 
 console.log(`\nyou can 'npm start' now`)
